ce52479e9545f28bc4048e64aef9e2c5b622d8ab
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-01-09 20:08:30 -0300

    FCC front end 2/5

---
+++ Front End Development Libraries/build-a-markdown-previewerfreecodecamp/README.md
@@ -0,0 +1,4 @@
+# Build a Markdown Previewer - FreeCodeCamp
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Oaluiser/pen/XWBpqQj](https://codepen.io/Oaluiser/pen/XWBpqQj).
+
---
+++ Front End Development Libraries/build-a-markdown-previewerfreecodecamp/src/script.js
@@ -0,0 +1,60 @@
+import React from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+class Previewer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialText: `# This is a React Markdown Previewer made by Aluisio Pereira using React!
+      
+You can check my GitHub [here.](https://github.com/Oaluiser)
+
+## This is a example of a Sub Heading.
+
+Heres some inline code, \`<div></div>\`.
+
+\`\`\`
+// this is multi-line code:
+let portfolioLink = "https://github.com/Oaluiser/Portfolio"
+if (interest === true) {
+  return portfolioLink
+}
+\`\`\`
+
+- This is a list example.
+  - With bullets.
+     - One more indentation level.
+        
+> This is a Block Quote.
+
+And this is... **me!**.
+
+![Aluisio Pereira](https://avatars.githubusercontent.com/u/109995434?s=400&u=17829bbd2634ff1093a1e8ac69523fcd9ab5f8b0&v=4)`
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  componentDidMount() {
+    document.getElementById('editor').innerHTML = this.state.initialText
+    document.getElementById('preview').innerHTML = marked.parse(this.state.initialText)
+  }
+  
+  handleChange(event) {
+    document.getElementById('preview').innerHTML =
+      marked.parse(event.target.value)
+  }
+  
+  render() {
+    return (
+      <div id="container" class="container">
+        <textarea id="editor" onChange={this.handleChange} class="form-control mb-5 mt-2"></textarea>
+        <div id="preview"></div>
+      </div>
+    )
+  };
+};
+
+
+root.render(<Previewer />);
