202fb7ac03b495777716b24deaa6535ec30117ac
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-01-11 21:37:35 -0300

    Not finished - 15/16 tests

---
+++ Front End Development Libraries/build-a-javascript-calculatorfreecodecamp/README.md
@@ -0,0 +1,4 @@
+# Build a JavaScript Calculator - FreeCodeCamp
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Oaluiser/pen/XWBMBGo](https://codepen.io/Oaluiser/pen/XWBMBGo).
+
---
+++ Front End Development Libraries/build-a-javascript-calculatorfreecodecamp/dist/script.js
@@ -0,0 +1,136 @@
+import React from 'https://esm.sh/react@18.2.0';
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0';
+
+const root = ReactDOM.createRoot(
+document.getElementById('root'));
+
+
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    document.getElementById("display").innerHTML = "0";
+  }
+
+  handleClick(event) {
+    console.log(document.getElementById(event.target.id).innerHTML);
+
+    const mathRegex = new RegExp('[*+\/-]');
+    const mathRegexWithoutMinus = new RegExp('[*+\/]');
+    const updateDisplay = html => {
+      if (html === ".") {
+        let arr = document.getElementById("display").innerHTML.split("");
+        console.log(arr);
+      }
+      if (document.getElementById("display").innerHTML === "0" && html === ".") {
+        document.getElementById("display").innerHTML = "0.";
+      } else if (document.getElementById("display").innerHTML === "0") {
+        document.getElementById("display").innerHTML = html;
+      } else if (document.getElementById("display").innerHTML.slice(-1) === "." && html === ".") {
+
+      } else if (mathRegexWithoutMinus.test(document.getElementById("display").innerHTML.slice(-1)) === true && html === "-") {
+        document.getElementById("display").innerHTML += html;
+      } else if (mathRegex.test(document.getElementById("display").innerHTML.slice(-1)) === true && mathRegex.test(html) === true) {
+        if (document.getElementById("display").innerHTML.slice(-1) === "-") {
+          let lastCharacterRemoved = document.getElementById("display").innerHTML.slice(0, -1).slice(0, -1);
+          document.getElementById("display").innerHTML = lastCharacterRemoved + html;
+          console.log("entrou");
+        }
+        let lastCharacterRemoved = document.getElementById("display").innerHTML.slice(0, -1);
+        document.getElementById("display").innerHTML = lastCharacterRemoved + html;
+      } else {
+        document.getElementById("display").innerHTML += html;
+      }
+    };
+
+    const clearDisplay = () => {
+      document.getElementById("display").innerHTML = "0";
+    };
+
+    const equals = () => {
+      document.getElementById("display").innerHTML = eval(document.getElementById("display").innerHTML);
+    };
+
+    switch (event.target.id) {
+      case "clear":
+        clearDisplay();
+        break;
+      case "decimal":
+      case "zero":
+      case "one":
+      case "two":
+      case "three":
+      case "four":
+      case "five":
+      case "six":
+      case "seven":
+      case "eight":
+      case "nine":
+      case "add":
+      case "subtract":
+      case "multiply":
+      case "divide":
+        updateDisplay(document.getElementById(event.target.id).innerHTML);
+        break;
+      case "equals":
+        equals();
+        break;
+      default:
+        return event.target.id;}
+
+  }
+
+
+  render() {
+    return /*#__PURE__*/(
+      React.createElement("div", { id: "calculator", class: "container text-center bg-light p-4" }, /*#__PURE__*/
+      React.createElement("div", { id: "displaycontainer", class: "row bg-dark" }, /*#__PURE__*/React.createElement("p", { id: "display", class: "text-end text-light mb-0" })), /*#__PURE__*/
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "clear", class: "col-6 btn btn-danger", onClick: this.handleClick }, "AC"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "divide", class: "col btn btn-secondary", onClick: this.handleClick }, "/"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "multiply", class: "col btn btn-secondary", onClick: this.handleClick }, "*")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("div", { class: "col-9" }, /*#__PURE__*/
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "seven", class: "col btn btn-primary", onClick: this.handleClick }, "7"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "eight", class: "col btn btn-primary", onClick: this.handleClick }, "8"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "nine", class: "col btn btn-primary", onClick: this.handleClick }, "9")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "four", class: "col btn btn-primary", onClick: this.handleClick }, "4"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "five", class: "col btn btn-primary", onClick: this.handleClick }, "5"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "six", class: "col btn btn-primary", onClick: this.handleClick }, "6")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "one", class: "col btn btn-primary", onClick: this.handleClick }, "1"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "two", class: "col btn btn-primary", onClick: this.handleClick }, "2"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "three", class: "col btn btn-primary", onClick: this.handleClick }, "3")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "zero", class: "col btn btn-primary", onClick: this.handleClick }, "0"), /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "decimal", class: "col btn btn-primary", onClick: this.handleClick }, "."))), /*#__PURE__*/
+
+
+      React.createElement("div", { class: "col-3" }, /*#__PURE__*/
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "subtract", class: "col btn btn-secondary", onClick: this.handleClick }, "-")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "add", class: "col btn btn-secondary", onClick: this.handleClick }, "+")), /*#__PURE__*/
+
+      React.createElement("div", { class: "row" }, /*#__PURE__*/
+      React.createElement("button", { type: "button", id: "equals", class: "col btn btn-dark", onClick: this.handleClick }, "="))))));
+
+
+
+
+
+  }}
+
+
+root.render( /*#__PURE__*/React.createElement(Calculator, null));
