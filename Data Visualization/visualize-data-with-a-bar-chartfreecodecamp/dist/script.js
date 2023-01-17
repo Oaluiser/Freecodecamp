d02194858053f9f595f57b62b744fd6fa959a4b9
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-01-17 17:21:01 -0300

    ex 1/5 done

--- Data Visualization/visualize-data-with-a-bar-chartfreecodecamp/dist/script.js
+++ Data Visualization/visualize-data-with-a-bar-chartfreecodecamp/dist/script.js
@@ -4,6 +4,13 @@ const w = 800;
 const h = 400;
 const padding = 50;
 
+let tooltip = d3.select("#container").
+append("div").
+attr("id", "tooltip").
+style("opacity", 0).
+style("height", "auto").
+style("width", "auto");
+
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json").then(data => {
   const dataset = data.data;
 
@@ -12,8 +19,8 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
   range([padding, w - padding]);
 
   const yScale = d3.scaleLinear().
-  domain([d3.max(dataset, d => d[1]), d3.min(dataset, d => d[1])]).
-  range([10, h - padding]);
+  domain([0, d3.max(dataset, d => d[1])]).
+  range([h - padding, 0]);
 
   const xAxis = d3.axisBottom(xScale);
 
@@ -42,8 +49,19 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
   attr("class", "bar").
   attr("data-date", (d, i) => dataset[i][0]).
   attr("data-gdp", (d, i) => dataset[i][1]).
-  attr("x", 0).
-  attr("y", h - padding).
-  attr("width", w / 275).
-  attr("height", (d, i) => data.data[i][1]);
+  attr("x", (d, i) => xScale(Date.parse(data.data[i][0]))).
+  attr("y", (d, i) => yScale(data.data[i][1])).
+  attr("width", w / dataset.length).
+  attr("height", (d, i) => h - padding - yScale(data.data[i][1])).
+  on("mouseover", (e, d, i) => {
+    tooltip.transition().
+    style("opacity", "1");
+
+    tooltip.text(d[0]).
+    attr("data-date", d[0]);
+  }).
+  on("mouseout", (d, i) => {
+    tooltip.transition().
+    style("opacity", "0");
+  });
 });
