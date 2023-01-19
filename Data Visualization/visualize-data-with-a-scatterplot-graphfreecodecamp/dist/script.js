90882ed462985fe24380d32f0636f497d3c12791
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-01-19 03:24:31 -0300

    Ex 2/5 finished

--- Data Visualization/visualize-data-with-a-scatterplot-graphfreecodecamp/dist/script.js
+++ Data Visualization/visualize-data-with-a-scatterplot-graphfreecodecamp/dist/script.js
@@ -6,6 +6,15 @@ const p = 40;
 const timeFormat = d3.timeFormat("%M:%S");
 const doppingColor = "steelblue";
 const noDoppingColor = "tomato";
+const tooltipContainer = d3.select("body").
+append("div").
+attr("id", "tooltip").
+style("background-color", "lightsteelblue").
+style("border-radius", "10px").
+style("padding", "10px").
+style("position", "absolute").
+style("font-size", "0.9em").
+style("opacity", 0);
 
 d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json").then(data => {
   data.forEach(d => {
@@ -59,7 +68,19 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
     }
   }).
   style("opacity", 0.9).
-  style("stroke", "black");
+  style("stroke", "black").
+  on("mouseover", (event, d) => {
+    tooltipContainer.style("opacity", 0.9).
+    html(`${d.Name}: ${d.Nationality} <br> Year: ${d.Year}, Time: ${d.Time.getMinutes()}:${d.Time.getSeconds() < 10 ? d.Time.getSeconds() + "0" : d.Time.getSeconds()} ${d.Doping === "" ? "" : "<br><br>" + d.Doping}`).
+    style("top", event.pageY - 30 + "px").
+    style("left", event.pageX + "px").
+    attr("data-year", d.Year);
+  }).
+  on("mouseout", (event, d) => {
+    tooltipContainer.style("opacity", 0).
+    style("top", "0px").
+    style("left", "0px");
+  });
 
   const legendContainer = svg.append("g").attr("id", "legend");
 
@@ -88,15 +109,6 @@ d3.json("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/mas
   attr("y", 164).
   text("Riders with doping allegations").
   style("font-size", "15px");
-
-  const tooltipContainer = svg.select("#tooltipContainer").attr("id", "tooltip");
-
-  tooltipContainer.append("rect").
-  attr("x", 100).
-  attr("y", 100).
-  attr("width", 100).
-  attr("height", 100).
-  style("fill", doppingColor);
 
 
 });
