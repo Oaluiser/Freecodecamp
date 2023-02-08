73e52d65a2370e8501b9bc7f35a5e5c913791d04
Aluisio Pereira <109995434+Oaluiser@users.noreply.github.com>
2023-02-07 22:51:49 -0300

    Salon Appointment Scheduler Finished

--- Relational Database/Salon Appointment Scheduler/salon.sh
+++ Relational Database/Salon Appointment Scheduler/salon.sh
@@ -1,1 +1,46 @@
 #!/bin/bash
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
+
+echo -e "\n~~~~~ MY SALON ~~~~~\n"
+
+# main menu
+MAIN_MENU() {
+  SERVICES=$($PSQL "SELECT * FROM services;")
+  echo "$SERVICES" | while read SERVICE_ID BAR NAME
+    do
+      echo "$SERVICE_ID) $NAME"
+    done
+
+  read SERVICE_ID_SELECTED
+
+  if [[ ! $SERVICE_ID_SELECTED =~ ^[1-3]+$ ]]
+  then
+    MAIN_MENU "That is not a valid service."
+  else
+    SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id = $SERVICE_ID_SELECTED;")
+    if [[ -z $SERVICE_NAME ]]
+    then
+      MAIN_MENU "I could not find that service. What would you like today?"
+    else
+      echo "What's your phone number?"
+      read CUSTOMER_PHONE
+      CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone = '$CUSTOMER_PHONE';")
+
+      if [[ -z $CUSTOMER_NAME ]]
+      then
+        echo "I don't have a record for that phone number, what's your name?"
+        read CUSTOMER_NAME
+        INSERT_CUSTOMER=$($PSQL "INSERT INTO customers(phone, name) VALUES('$CUSTOMER_PHONE', '$CUSTOMER_NAME')")
+        CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone = '$CUSTOMER_PHONE';")
+      fi
+
+      echo "What time would you like your$SERVICE_NAME, $CUSTOMER_NAME?"
+      read SERVICE_TIME
+
+      INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, time, service_id) VALUES($CUSTOMER_ID, '$SERVICE_TIME', $SERVICE_ID_SELECTED)")
+      echo "I have put you down for a$SERVICE_NAME at $SERVICE_TIME, $CUSTOMER_NAME."
+    fi
+  fi
+}
+
+MAIN_MENU
